f526d0e824c1e0c8b532c4b4baf06397e631ec48
DESKTOP-IVN453T\Admin <viendinhanh0401@gmail.com>
2025-10-22 17:23:33 +0700

    update

---
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+        // ViewResolver cho JSP
+        @Bean
+        public InternalResourceViewResolver viewResolver() {
+            InternalResourceViewResolver vr = new InternalResourceViewResolver();
+            vr.setViewClass(JstlView.class);
+            vr.setPrefix("/WEB-INF/view/");  // đúng với cấu trúc thư mục ở trên
+            vr.setSuffix(".jsp");
+            return vr;
+        }
+
+        // (tuỳ chọn) map tài nguyên tĩnh nếu bạn đặt ngoài /static, /public
+        @Override
+        public void configureViewResolvers(ViewResolverRegistry registry) {
+            registry.viewResolver(viewResolver());
+
+        }
+
+    }
+
+
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//public class UserController {
+//
+//    private UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/")
+//    public String getHomePage(){
+//        return this.userService.handleService();
+//    }
+//}
+@Controller
+public class UserController {
+
+    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleService();
+        return "hello";
+    }
+}
